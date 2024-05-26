ad0d9f03f608f93ffdb496922eedec0e73ae5750
Anıl <anilkaramis@gmail.com>
2024-05-26 18:26:41 +0300

    Update: DiscoverViewController.swift

--- RxFilm/DiscoverView/DiscoverViewController.swift
+++ RxFilm/DiscoverView/DiscoverViewController.swift
@@ -14,8 +14,9 @@ class DiscoverViewController:UIViewController {
     
     lazy var CollectionView : UICollectionView = {
         var flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        
+        flowLayout.sectionInset = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        flowLayout.itemSize = CGSize(width: 150, height: 150)
+        flowLayout.headerReferenceSize = CGSize(width: self.preferredContentSize.width, height: 180)
         return UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
     }()
     
@@ -28,16 +29,21 @@ class DiscoverViewController:UIViewController {
     }
 }
 
-//MARK: Draw UI
+//MARK: -Draw UI
+
 extension DiscoverViewController {
    
     private func initUI() {
+        
         super.view.addSubview(CollectionView)
+        
         CollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: Identifiers.discover_collection_cell)
+        CollectionView.register(DiscoverCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Identifiers.discover_collection_header)
         CollectionView.backgroundColor = UIColor(named: Colors.background)
         CollectionView.snp.makeConstraints { $0.edges.equalToSuperview()}
     }
 }
+//MARK: -Collection View Configuration
 
 extension DiscoverViewController:UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -45,12 +51,19 @@ extension DiscoverViewController:UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        //Cell Property
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.discover_collection_cell, for: indexPath)
         cell.backgroundColor = .systemRed
         return cell
     }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: Identifiers.discover_collection_header, for: indexPath)
+    }
 }
-extension DiscoverViewController:UICollectionViewDelegate {
+extension DiscoverViewController:UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         printContent(indexPath.row)
     }
