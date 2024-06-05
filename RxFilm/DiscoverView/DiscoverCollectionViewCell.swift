459291b7bf0948b0c1fe5607c152a1e78284d59e
Anıl <anilkaramis@gmail.com>
2024-06-05 23:48:00 +0300

    Update DiscoverCollectionViewCell.swift

--- RxFilm/DiscoverView/DiscoverCollectionViewCell.swift
+++ RxFilm/DiscoverView/DiscoverCollectionViewCell.swift
@@ -9,21 +9,60 @@ import UIKit
 import SnapKit
 
 class DiscoverCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: Properties
+    
     let posterImage : UIImageView = {
         lazy var imageView = UIImageView()
         imageView.image = UIImage(named: "img_placeholder")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
+    
     let movieTitle : UILabel = {
         lazy var text = UILabel()
         text.font = UIFont.systemFont(ofSize: 20)
         text.textColor = .white
         return text
     }()
-    func insertData (imageURLString: String, title: String) {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(movieTitle)
+        self.addSubview(posterImage)
+        
+        //MARK: Constraints Properties
+        
+        posterImage.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+        }
+        
+        movieTitle.snp.makeConstraints {
+            $0.top.equalTo(posterImage.snp.bottom).offset(15)
+            $0.bottom.greaterThanOrEqualToSuperview()
+            $0.leading.equalTo(posterImage.snp.leading)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+//MARK: -Data layer
+extension DiscoverCollectionViewCell {
+    
+    func insertData(imageURLString:String, title:String) {
+        
+        movieTitle.text = title
+        
         DispatchQueue.global().async {
-            guard let imageURL = URL(string: imageURLString) else { return }
+            guard let imageURL = URL(string: imageURLString) else {
+                return }
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            
+            DispatchQueue.main.sync {
+                self.posterImage.image = UIImage(data:imageData)
+            }
         }
     }
 }
