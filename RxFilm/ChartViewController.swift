6b653c253cb6b0eaf938e5e2c9ef50d9e21437ef
Anıl <anilkaramis@gmail.com>
2024-06-06 23:43:56 +0300

    Add TabBarController

---
+++ RxFilm/ChartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChartViewController.swift
+//  RxFilm
+//
+//  Created by AnılKaramış on 6.06.2024.
+//
+
+import Foundation
+import UIKit
+
+class ChartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
+
