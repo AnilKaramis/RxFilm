2731355d9488c3d5c4ae1095902bf521d85f4e7c
Anıl <anilkaramis@gmail.com>
2024-06-22 19:59:42 +0300

    Add Model

---
+++ RxFilm/Domain/Model/MovieFront.swift
@@ -0,0 +1,18 @@
+//
+//  MovieFront.swift
+//  RxFilm
+//
+//  Created by AnılKaramış on 22.06.2024.
+//
+
+import Foundation
+
+struct MovieFront {
+    let title: String
+    let posterPath: String
+    
+    let genre: String
+    let releaseDate: String
+    let ratingScore: Double
+    let ratingCount: Int
+}
---
+++ RxFilm/Domain/Service/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  RxFilm
+//
+//  Created by AnılKaramış on 22.06.2024.
+//
+
+import Foundation
